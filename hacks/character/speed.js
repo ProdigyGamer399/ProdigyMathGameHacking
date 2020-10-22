edda9ddcab45ecd76d7bcd1ff3ebc7f120b80103
ProdigyGamer399 <73310041+ProdigyGamer399@users.noreply.github.com>
2020-10-22 11:48:27 -0700

    Update speed.js

--- hacks/character/speed.js
+++ hacks/character/speed.js
@@ -1,3 +1,3 @@
 //  Makes you walk fast
-_.instance.prodigy.game.state.states.DinoDig.walkSpeed = 100;
-_.instance.prodigy.user.walkSpeed = 10;
+_.instance.prodigy.game.state.states.DinoDig.walkSpeed = 10000;
+_.instance.prodigy.user.walkSpeed = 10000;
